11ccd29e1d758b2468c3937dedb2aca4dfb8f6b9
Mike van Niehoff <mikevanniehoff@example.com>
2020-09-15 12:53:19 +0200

    Tweede Test toegevoegd nu voor punt naar beneden

--- src/test/java/org/example/mike/MijnEersteHuiswerk15092020/MijnEersteHuiswerkOpdracht.java
+++ src/test/java/org/example/mike/MijnEersteHuiswerk15092020/MijnEersteHuiswerkOpdracht.java
@@ -6,7 +6,7 @@ public class MijnEersteHuiswerkOpdracht {
 
     //Test die drie simpele pyramides met punt naar boven print in verschillende niveaus
     @Test
-    void PrintPyramidesMetPuntNaarBovenVanTweeDrieEnVijfNiveaus(){
+    void Print2DPyramidesMetPuntNaarBovenVanTweeDrieEnVijfNiveaus(){
         printPyramideMetPuntNaarBovenVanTweeNiveaus();
         voegWitRegelToe();
 
@@ -16,11 +16,32 @@ public class MijnEersteHuiswerkOpdracht {
         printPyramideMetPuntNaarBovenVanVijfNiveaus();
     }
 
+    @Test
+    void Print2DPyramidesMetPuntNaarBenedenVanTweeDrieEnVijfNiveaus(){
+        printPyramideMetPuntNaarBenedenVanTweeNiveaus();
+        voegWitRegelToe();
+
+        printPyramideMetPuntNaarBenedenVanDrieNiveaus();
+        voegWitRegelToe();
+
+        printPyramideMetPuntNaarBenedenVanVijfNiveaus();
+    }
+
+    @Test
+    void Print2DPyramidesMetPuntNaarRechtsVanTweeDrieEnVijfNiveaus(){
+    }
+
+    @Test
+    void Print2DPyramidesMetPuntNaarLinksVanTweeDrieEnVijfNiveaus(){
+
+    }
+
     //Methodes
     void voegWitRegelToe(){
         System.out.println("");
     }
 
+    //punt naar boven
     void printPyramideMetPuntNaarBovenVanTweeNiveaus(){
         System.out.println(" + ");
         System.out.println("+++");
@@ -38,5 +59,25 @@ public class MijnEersteHuiswerkOpdracht {
         System.out.println("  +++++  ");
         System.out.println(" +++++++ ");
         System.out.println("+++++++++");
+    }
+
+    //punt naar beneden
+    void printPyramideMetPuntNaarBenedenVanTweeNiveaus(){
+        System.out.println("+++");
+        System.out.println(" + ");
+    }
+
+    void printPyramideMetPuntNaarBenedenVanDrieNiveaus(){
+        System.out.println("+++++");
+        System.out.println(" +++ ");
+        System.out.println("  +  ");
+    }
+
+    void printPyramideMetPuntNaarBenedenVanVijfNiveaus(){
+        System.out.println("+++++++++");
+        System.out.println(" +++++++ ");
+        System.out.println("  +++++  ");
+        System.out.println("   +++   ");
+        System.out.println("    +    ");
     }
 }
