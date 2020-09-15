a1a9ae03e944dab459a355b646b1d3092e3ffd61
Mike van Niehoff <mikevanniehoff@example.com>
2020-09-15 11:45:00 +0200

    Eerste Test toegevoegd

--- src/test/java/org/example/mike/MijnEersteHuiswerk15092020/MijnEersteHuiswerkOpdracht.java
+++ src/test/java/org/example/mike/MijnEersteHuiswerk15092020/MijnEersteHuiswerkOpdracht.java
@@ -4,8 +4,39 @@ import org.junit.jupiter.api.Test;
 
 public class MijnEersteHuiswerkOpdracht {
 
+    //Test die drie simpele pyramides met punt naar boven print in verschillende niveaus
     @Test
-    void mijnEersteMethode(){
+    void PrintPyramidesMetPuntNaarBovenVanTweeDrieEnVijfNiveaus(){
+        printPyramideMetPuntNaarBovenVanTweeNiveaus();
+        voegWitRegelToe();
 
+        printPyramideMetPuntNaarBovenVanDrieNiveaus();
+        voegWitRegelToe();
+
+        printPyramideMetPuntNaarBovenVanVijfNiveaus();
+    }
+
+    //Methodes
+    void voegWitRegelToe(){
+        System.out.println("");
+    }
+
+    void printPyramideMetPuntNaarBovenVanTweeNiveaus(){
+        System.out.println(" + ");
+        System.out.println("+++");
+    }
+
+    void printPyramideMetPuntNaarBovenVanDrieNiveaus(){
+        System.out.println("  +  ");
+        System.out.println(" +++ ");
+        System.out.println("+++++");
+    }
+
+    void printPyramideMetPuntNaarBovenVanVijfNiveaus(){
+        System.out.println("    +    ");
+        System.out.println("   +++   ");
+        System.out.println("  +++++  ");
+        System.out.println(" +++++++ ");
+        System.out.println("+++++++++");
     }
 }
