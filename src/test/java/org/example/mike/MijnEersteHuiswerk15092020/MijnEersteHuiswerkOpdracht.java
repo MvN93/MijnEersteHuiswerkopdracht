13188eb942d149997536a1ce3498b1e13eecbdba
Mike van Niehoff <mikevanniehoff@example.com>
2020-09-15 10:56:10 +0200

    eerste commit van eerste huiswerk opdracht

---
+++ src/test/java/org/example/mike/MijnEersteHuiswerk15092020/MijnEersteHuiswerkOpdracht.java
@@ -0,0 +1,11 @@
+package org.example.mike.MijnEersteHuiswerk15092020;
+
+import org.junit.jupiter.api.Test;
+
+public class MijnEersteHuiswerkOpdracht {
+
+    @Test
+    void mijnEersteMethode(){
+
+    }
+}
